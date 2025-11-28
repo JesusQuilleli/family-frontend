d9df2f2c450de71f61ec972afc6605933496b514
Yissus <119908576+JesusQuilleli@users.noreply.github.com>
2025-11-28 19:14:35 -0400

    version mobile pwa v1

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Family App Shop',
+        short_name: 'FamilyShop',
+        description: 'La mejor tienda para la familia',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'tienda.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'tienda.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
